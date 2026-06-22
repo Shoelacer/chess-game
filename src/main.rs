646694363535f19f569c11caf97ce9fb4612c0af
Shoelacer <139281278+Shoelacer@users.noreply.github.com>
2026-06-22 11:20:57 -0500

    Adds print_board_pretty function to print the board in a readable format

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,20 @@ fn main() {
 
     game.make_move(ChessMove::from_san(&game.current_position(), "e4").expect("Not valid"));
 
-    println!("{}", game.current_position().to_string());
+    print_board_pretty(&game.current_position());
+}
+
+fn print_board_pretty(board: &chess::Board) {
+    let board = board.to_string();
+    for i in board.chars() {
+        if let Ok(i) = i.to_string().parse() {
+            for _ in 0..i {
+                print!("*");
+            }
+        } else if i == '/' {
+            println!();
+        } else {
+            print!("{i}");
+        }
+    }
 }
