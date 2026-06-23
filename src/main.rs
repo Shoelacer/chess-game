10509a18169664130b083a69c4525c10c3b8695f
Shoelacer <139281278+Shoelacer@users.noreply.github.com>
2026-06-23 11:29:04 -0500

    Builds the playing loop with move info and move requests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use chess::{ChessMove, File, Game, Rank, Square};
+use std::{panic::PanicHookInfo, str::FromStr};
 
-fn main() {
+use chess::{ChessMove, Color, File, Game, Rank, Square};
+use tokio::{self};
+
+#[tokio::main]
+async fn main() {
     let mut game = Game::new();
 
-    println!("{}", game.current_position().to_string());
-
-    game.make_move(ChessMove::from_san(&game.current_position(), "e4").expect("Not valid"));
-
     print_board_pretty(&game.current_position());
+    while game.result().is_none() {
+        print_current_move_info(&game);
+        game.make_move(request_move(&game));
+        print_board_pretty(&game.current_position());
+    }
+    println!("{:?}", game.result().unwrap());
 }
 
 fn print_board_pretty(board: &chess::Board) {
@@ -26,4 +32,35 @@ fn print_board_pretty(board: &chess::Board) {
             print!("{i}");
         }
     }
+}
+
+fn print_current_move_info(game: &Game) {
+    if game.side_to_move() == Color::White {
+        println!("White's Turn");
+    } else {
+        println!("Black's Turn");
+    }
+    if (game.current_position().checkers().popcnt() > 0) {
+        println!("Currently in check");
+    }
+}
+
+fn request_move(game: &Game) -> ChessMove {
+    let mut input = String::new();
+    let mut move_coords = "";
+
+    while ChessMove::from_san(&game.current_position(), move_coords).is_err() {
+        input.clear();
+        println!("Enter a valid move: ");
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Couldn't read the move");
+        move_coords = input.trim_end();
+    }
+    println!(
+        "Move: {}",
+        ChessMove::from_san(&game.current_position(), move_coords).unwrap()
+    );
+
+    ChessMove::from_san(&game.current_position(), &move_coords).unwrap()
 }
