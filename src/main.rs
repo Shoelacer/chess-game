1eed8f71f3445da1f5dd1c2ed1fb649084c77fda
Shoelacer <139281278+Shoelacer@users.noreply.github.com>
2026-06-25 10:50:14 -0500

    Implements rudimentary logic of sending moves from clients

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use chess::{ChessMove, Color, File, Game, Rank, Square};
+use futures_util::stream::SplitSink;
+use futures_util::{SinkExt, StreamExt};
 use std::{panic::PanicHookInfo, str::FromStr};
+use tokio::io::AsyncReadExt;
 use tokio::net::TcpStream;
+use tokio::net::tcp::ReadHalf;
 use tokio::{self, net::TcpListener};
+use tokio_tungstenite::WebSocketStream;
 use tokio_tungstenite::accept_async;
 use tokio_tungstenite::tungstenite::Message as WsMessage;
 
@@ -17,20 +22,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (stream2, addr2) = listener.accept().await?;
     println!("Player 2 connected: {}", addr2);
 
-    let ws1 = accept_async(stream1).await?.into_inner();
-    let ws2 = accept_async(stream2).await?.into_inner();
+    let mut ws1 = accept_async(stream1).await?;
+    let mut ws2 = accept_async(stream2).await?;
 
-    play_game(ws1, ws2);
+    play_game(ws1, ws2).await;
 
     Ok(())
 }
 
-fn play_game(player_one: TcpStream, player_two: TcpStream) {
+async fn play_game(
+    mut player_one: WebSocketStream<TcpStream>,
+    mut player_two: WebSocketStream<TcpStream>,
+) {
     let mut game = Game::new();
+    //let (mut read_one, mut write_one) = player_one.split();
+    //let (mut read_two, mut write_two) = player_two.split();
     print_board_pretty(&game.current_position());
     while game.result().is_none() {
         print_current_move_info(&game);
-        game.make_move(request_move(&game));
+        if game.side_to_move() == Color::White {
+            game.make_move(request_move(&game, &mut player_one).await);
+        } else {
+            game.make_move(request_move(&game, &mut player_two).await);
+        }
         print_board_pretty(&game.current_position());
     }
     println!("{:?}", game.result().unwrap());
@@ -65,22 +79,30 @@ fn print_current_move_info(game: &Game) {
     }
 }
 
-fn request_move(game: &Game) -> ChessMove {
+async fn request_move(game: &Game, player: &mut WebSocketStream<TcpStream>) -> ChessMove {
     let mut input = String::new();
     let mut move_coords = "";
 
-    while ChessMove::from_san(&game.current_position(), move_coords).is_err() {
+    /*while ChessMove::from_san(&game.current_position(), move_coords).is_err() {
         input.clear();
         println!("Enter a valid move: ");
         std::io::stdin()
             .read_line(&mut input)
             .expect("Couldn't read the move");
         move_coords = input.trim_end();
-    }
-    println!(
-        "Move: {}",
-        ChessMove::from_san(&game.current_position(), move_coords).unwrap()
-    );
+    }*/
+    //let mut move_coords = ChessMove::from_san(&game.current_position(), "a3").unwrap();
 
-    ChessMove::from_san(&game.current_position(), &move_coords).unwrap()
+    while let Some(msg) = player.next().await {
+        let msg = msg.unwrap();
+
+        if let WsMessage::Text(text) = msg {
+            println!("Received move: {}", text);
+
+            return ChessMove::from_san(&game.current_position(), text.trim())
+                .expect("Invalid move");
+        }
+    }
+
+    panic!("Client disconnected");
 }
